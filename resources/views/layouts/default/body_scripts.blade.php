7cd5e0c9daeea77992a560ba9827757fde345018
Rafael Dória <ieieldoria@hotmail.com>
2018-02-19 22:29:05 -0300

    Atualização scripts.

--- resources/views/layouts/default/body_scripts.blade.php
+++ resources/views/layouts/default/body_scripts.blade.php
@@ -1,5 +1,4 @@
 <script src="js/manifest.js"></script>
 <script src="js/vendor.js"></script>
 <script src="js/bootstrap.js"></script>
-<script src="js/threads.js"></script>
-<script src="js/replies.js"></script>
+{{$slot}}
